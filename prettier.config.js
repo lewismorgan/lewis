dc7570f45d12d9128a18f56eb4870cdbe58e6766
Lewis <lewismorgan1995@icloud.com>
2024-04-24 20:04:01 -0400

    fix prettier for nextjs

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-packagejson"],
+  plugins: ['prettier-plugin-packagejson', 'prettier-plugin-tailwindcss'],
   tabWidth: 2,
   useTabs: false,
   semi: false,
   singleQuote: true,
-  trailingComma: "es5",
-};
+  trailingComma: 'es5',
+}
 
-export default config;
+export default config
