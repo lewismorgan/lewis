1da7c81c73adf2b19d189dbecad798032722578e
Lewis <lewismorgan1995@icloud.com>
2024-04-29 17:13:37 -0400

    fix tailwind build errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,10 +79,7 @@ const config = {
       },
     },
   },
-  plugins: [
-    require('tailwindcss-animate'),
-    require('tailwindcss-hero-patterns'),
-  ],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config
 
 export default config
