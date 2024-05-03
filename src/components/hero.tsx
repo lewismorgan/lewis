30fb7a906c96dfdbe84d7d78b2ed77e62e538eaa
Lewis <lewismorgan1995@icloud.com>
2024-05-03 01:02:36 -0400

    Slight change to hero to remove punctuation

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,21 +1,25 @@
 const Spiel = () => {
   return (
-    <span className="hover:cursor-default">
-      The digital space, domain, and realm of Lewis Morgan. <br /> You may find
-      a lot of{' '}
-      <span className="relative">
-        <span className="absolute -inset-0 block -skew-y-6 rounded-full bg-gradient-to-r from-transparent to-blue-400 hover:animate-pulse dark:to-red-800"></span>
-        <span className="relative font-semibold hover:animate-pulse dark:text-white">
-          glowsticks
+    <div className="space-y-2 hover:cursor-default">
+      <div className="text-lg tracking-tight">
+        Welcome to the digital space, domain, and realm of Lewis Morgan
+      </div>
+      <div>
+        You may find a lot of{' '}
+        <span className="relative">
+          <span className="absolute -inset-0 block -skew-y-6 rounded-full bg-gradient-to-r from-transparent to-blue-400 hover:animate-pulse dark:to-red-800"></span>
+          <span className="relative font-semibold hover:animate-pulse dark:text-white">
+            glowsticks
+          </span>
         </span>
-      </span>
-      ,{' '}
-      <span className="text-nowrap tracking-wide text-green-800 underline decoration-wavy decoration-1 underline-offset-2">
-        space-lizards
-      </span>
-      , and lines of <span className="font-mono tracking-wider">code</span>{' '}
-      lying around.
-    </span>
+        ,{' '}
+        <span className="text-nowrap tracking-wide text-green-800 underline decoration-wavy decoration-1 underline-offset-2">
+          space-lizards
+        </span>
+        , and lines of <span className="font-mono tracking-wider">code</span>{' '}
+        lying around
+      </div>
+    </div>
   )
 }
 
